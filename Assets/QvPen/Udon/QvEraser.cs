7d3de412aeed16c98ce0e9650de6d4b160350962
ureishi <victorique2020+github@gmail.com>
2020-06-15 04:54:54 +0900

    QvPen追加

---
+++ Assets/QvPen/Udon/QvEraser.cs
@@ -0,0 +1,15 @@
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace ureishi.Udon.QvPen
+{
+    public class QvEraser : UdonSharpBehaviour
+    {
+        void Start()
+        {
+
+        }
+    }
+}
