bd244aded66e97575f95528938654277fd1d4322
mkc1370 <mkc1370go@gmail.com>
2020-06-22 21:29:40 +0900

    Reset AllにMasterの名前を表示するように変更

--- Assets/QvPen/Udon/UI/ResetAllButton.cs
+++ Assets/QvPen/Udon/UI/ResetAllButton.cs
@@ -1,5 +1,7 @@
+using System;
 using UdonSharp;
 using UnityEngine;
+using UnityEngine.UI;
 using VRC.SDKBase;
 using VRC.Udon.Common.Interfaces;
 
@@ -7,9 +9,36 @@ namespace QvPen.Udon.UI
 {
     public class ResetAllButton : UdonSharpBehaviour
     {
+        [SerializeField] private Text message;
         [SerializeField] private PenManager[] penManagers;
         [SerializeField] private EraserManager[] eraserManagers;
         
+        private VRCPlayerApi master;
+
+        public override void OnPlayerLeft(VRCPlayerApi player)
+        {
+            OnPlayerListChanged(player);
+        }
+
+        public override void OnPlayerJoined(VRCPlayerApi player)
+        {
+            OnPlayerListChanged(player);
+        }
+
+        private void OnPlayerListChanged(VRCPlayerApi player)
+        {
+            if (player.isMaster)
+            {
+                master = player;
+            }
+
+            var masterName = master == null ? "master" : master.displayName;
+            message.text =
+                $"<size=18>Reset All</size>\n\n" +
+                $"<size=8>[Only {masterName} can do]</size>\n\n" +
+                $"<size=14>(Global)</size>";
+        }
+
         public override void Interact()
         {
             if (!Networking.IsMaster) return;
